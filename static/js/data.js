d97c999f2cd843d0aeee3c4336f3e33e6245d2f5
hakeem235 <hakeemkoo@gmail.com>
2023-05-02 19:47:30 -0400

    fix the webpage

---
+++ static/js/data.js
@@ -0,0 +1,12 @@
+var data = [
+    {
+        Season:"2023" ,
+        playerName:"Ahmed Hakeem",
+        position: "L",
+        i_f_goals:45,
+        i_f_points:45,
+        i_f_penalityminutes:444,
+        i_f_hits:4444,
+        i_f_takeaways:4444,
+    }
+]
