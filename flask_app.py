844a31fa0c59ff039e79273def1ce58f2ad65265
hakeem235 <hakeemkoo@gmail.com>
2023-05-07 21:51:27 -0400

    add charts and add filters for the chart add 2 page one for ML and one for the chart

--- flask_app.py
+++ flask_app.py
@@ -6,13 +6,22 @@ import os
 app = Flask(__name__)
 
 
-
 # filepath = os.path.join("static", "js/data.json")
 # with open(filepath) as jsonfile:
 #     chartdata = json.load(jsonfile)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/analysis')
+def analysis():
+    return render_template('analysis.html')
+
+
+@app.route('/mlmodel')
+def ml_model():
+    return render_template('mlmodel.html')
 
 
 if __name__ == '__main__':
