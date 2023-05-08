bf8fe25595a1dee38eeee47041737097d9e9586e
hakeem235 <hakeemkoo@gmail.com>
2023-05-08 13:27:53 -0400

    deleted app.js change the chart from d3.js to ploty js

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,211 +1,102 @@
-// Define the initial filters
-let nameFilter = "All";
-let seasonFilter = "All";
-let teamFilter = "All";
+var names = data.map((d) => d.name);
+var seasons = data.map((d) => d.season);
+var teams = data.map((d) => d.team);
+var position = data.map((d) => d.position);
+var gamesPlayed = data.map((d) => d.games_played);
+var goals = data.map((d) => d.i_f_goals);
+var points = data.map((d) => d.i_f_points);
+var penalityMinutes = data.map((d) => d.i_f_penalityminutes);
+var hits = data.map((d) => d.i_f_hits);
+var takeaWays = data.map((d) => d.i_f_takeaways);
+var giveaWays = data.map((d) => d.i_f_giveaways);
+var oniceCorsiPercentage = data.map((d) => d.onice_corsipercentage);
+var oniceFenwickPercentage = data.map((d) => d.onice_fenwickpercentage);
+var gamesMissed = data.map((d) => d.games_missed);
 
-// Define the data selector and populate with unique names and seasons
-const uniqueNames = [...new Set(data.map((d) => d.name))];
-const uniqueSeasons = [...new Set(data.map((d) => d.season))];
-const uniqueTeams = [...new Set(data.map((d) => d.team))];
-uniqueNames.unshift("All");
-uniqueSeasons.unshift("All");
-uniqueTeams.unshift("All");
+var selector = d3.select("#name_select");
+names.forEach((name) => {
+  selector.append("option").text(name).property("value", name);
+});
+var selector = d3.select("#season_select");
+seasons.forEach((season) => {
+  selector.append("option").text(season).property("value", season);
+});
+var selector = d3.select("#team_select");
+teams.forEach((team) => {
+  selector.append("option").text(team).property("value", team);
+});
 
-// Add options to the name filter selector
-const nameFilterSelector = d3.select("#nameFilter");
-nameFilterSelector
-  .selectAll("option")
-  .data(uniqueNames)
-  .enter()
-  .append("option")
-  .text((d) => d)
-  .attr("value", (d) => d)
-  .property("selected", (d) => d === "All");
+var data = [
+  {
+    x: names,
+    y: points,
+    type: 'bar'
+  }
+];
 
-// Add options to the season filter selector
-const seasonFilterSelector = d3.select("#seasonFilter");
-seasonFilterSelector
-  .selectAll("option")
-  .data(uniqueSeasons)
-  .enter()
-  .append("option")
-  .text((d) => d)
-  .attr("value", (d) => d)
-  .property("selected", (d) => d === "All");
+Plotly.newPlot('myDiv', data);
 
-// Add options to the team filter selector
-const teamFilterSelector = d3.select("#teamFilter");
-teamFilterSelector
-  .selectAll("option")
-  .data(uniqueTeams)
-  .enter()
-  .append("option")
-  .text((d) => d)
-  .attr("value", (d) => d)
-  .property("selected", (d) => d === "All");
 
-// Define the dimensions of the chart
-const width = 1800;
-const height = 300;
-const margin = { top: 20, right: 30, bottom: 40, left: 50 };
+var trace1 = {
+  x: names,
+  y: gamesPlayed,
+  mode: 'markers',
+  type: 'scatter'
+};
 
-// Create the SVG element to hold the chart
-const svg = d3
-  .select("#chart")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+var trace2 = {
+  x: names,
+  y: gamesMissed,
+  mode: 'markers',
+  type: 'scatter'
+};
 
-// Define the scales for the x and y axes
-const xScale = d3
-  .scaleBand()
-  .range([0, width])
-  .domain(uniqueNames)
-  .padding(0.1);
+var trace3 = {
+  x: names ,
+  y: goals,
+  mode: 'markers',
+  type: 'scatter'
+};
 
-const yScale = d3
-  .scaleLinear()
-  .range([height, 0])
-  .domain([0, d3.max(data, (d) => d.i_f_points)]);
+var data = [trace1, trace2, trace3];
 
-// Define the color scale
-const colorScale = d3.scaleOrdinal().range(["steelblue", "orange"]);
+Plotly.newPlot('myDiv2', data);
 
-// Draw the bars on the chart
-svg
-  .selectAll("rect")
-  .data(
-    data.filter(
-      (d) =>
-        (nameFilter === "All" || d.name === nameFilter) &&
-        (seasonFilter === "All" || d.season === seasonFilter) &&
-        (teamFilter === "All" || d.team === teamFilter)
-    )
-  )
-  .enter()
-  .append("rect")
-  .attr("x", (d) => xScale(d.name))
-  .attr("y", (d) => yScale(d.i_f_points))
-  .attr("width", xScale.bandwidth())
-  .attr("height", (d) => height - yScale(d.i_f_points))
-  .attr("fill", (d) => colorScale(d.name));
 
-// Draw the x and y axes
-const xAxis = d3.axisBottom(xScale);
-const yAxis = d3.axisLeft(yScale);
+var x = seasons
 
-svg.append("g").attr("transform", `translate(0, ${height})`);
+var trace4 = {
+  y: gamesPlayed,
+  x: x,
+  name: 'games Played',
+  marker: {color: '#3D9970'},
+  type: 'box'
+};
 
-// Chart #2
-// Define the scales for the x, y, and radius axes
-const xScale2 = d3
-  .scaleBand()
-  .range([0, width])
-  .domain(data.map((d) => d.name))
-  .padding(0.1);
+var trace5 = {
+  y: gamesMissed,
+  x: x,
+  name: 'games Missed',
+  marker: {color: '#FF4136'},
+  type: 'box'
+};
 
-const yScale2 = d3
-  .scaleLinear()
-  .range([height, 0])
-  .domain([0, d3.max(data, (d) => d.games_played)]);
+var trace6 = {
+  y: points,
+  x: x,
+  name: 'points',
+  marker: {color: '#FF851B'},
+  type: 'box'
+};
 
-const rScale = d3
-  .scaleSqrt()
-  .range([5, 15])
-  .domain([0, d3.max(data, (d) => d.games_missed)]);
+var data = [trace4, trace5, trace6];
 
-// Create the SVG element to hold the chart
-const svg2 = d3
-  .select("#chart2")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+var layout = {
+  yaxis: {
+    title: '',
+    zeroline: false
+  },
+  boxmode: 'group'
+};
 
-// Draw the circles on the chart
-svg2
-  .selectAll("circle")
-  .data(
-    data.filter(
-      (d) =>
-      (nameFilter === "All" || d.name === nameFilter) &&
-      (seasonFilter === "All" || d.season === seasonFilter) &&
-      (teamFilter === "All" || d.team === teamFilter)
-    )
-  )
-  .enter()
-  .append("circle")
-  .attr("cx", (d) => xScale(d.name) + xScale.bandwidth() / 2)
-  .attr("cy", (d) => yScale(d.games_played))
-  .attr("r", (d) => rScale(d.games_missed))
-  .attr("fill", "steelblue");
-
-// Draw the x and y axes
-const xAxis2 = d3.axisBottom(xScale2);
-const yAxis2 = d3.axisLeft(yScale2);
-
-svg2.append("g").attr("transform", `translate(0, ${height})`).call(xAxis2);
-
-svg2.append("g").call(yAxis2);
-
-// Chart #3
-// Create the SVG element to hold the chart
-const svg3 = d3
-  .select("#chart3")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-// Define the scales for the x and y axes
-const xScale3 = d3
-  .scaleBand()
-  .range([0, width])
-  .domain(data.map((d) => d.name))
-  .padding(0.1);
-
-const yScale3 = d3
-  .scaleBand()
-  .range([height, 0])
-  .domain(data.map((d) => d.games_played))
-  .padding(0.1);
-
-// Define the color scale for the heatmap
-const colorScale2 = d3
-  .scaleSequential()
-  .interpolator(d3.interpolateRdYlBu)
-  .domain([
-    d3.min(data, (d) => d.games_played),
-    d3.max(data, (d) => d.games_played),
-  ]);
-
-// Draw the heatmap rectangles on the chart
-svg3
-  .selectAll("rect")
-  .data(
-    data.filter(
-      (d) =>
-      (nameFilter === "All" || d.name === nameFilter) &&
-      (seasonFilter === "All" || d.season === seasonFilter) &&
-      (teamFilter === "All" || d.team === teamFilter)
-    )
-  )
-  .enter()
-  .append("rect")
-  .attr("x", (d) => xScale(d.name))
-  .attr("y", (d) => yScale(d.games_played))
-  .attr("width", xScale3.bandwidth())
-  .attr("height", yScale3.bandwidth())
-  .attr("fill", (d) => colorScale(d.games_played));
-
-// Draw the x and y axes
-const xAxis3 = d3.axisBottom(xScale3);
-const yAxis3 = d3.axisLeft(yScale3);
-
-svg3.append("g").attr("transform", `translate(0, ${height})`).call(xAxis3);
-
-svg3.append("g").call(yAxis3);
+Plotly.newPlot('myDiv3', data, layout);
